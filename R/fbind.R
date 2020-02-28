ac2f13a1eb834a7cc8e2676ed973fd4cfeae852f
Andres Pitta <apitta6@gmail.com>
2020-02-27 23:42:28 -0800

    add fcount()

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,12 @@
+#' Herou can you hear me
+#'
+#' @param a herou
+#' @param b hero
+#'
+#' @return hero
+#' @export
+#'
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
